fe3e484b1ea91c5ca264730f8b1a1fb4bcbff2ae
wam <wam@onesty-tech.de>
2021-01-02 20:06:11 +0100

    publish frontend on heroku

--- frontend/src/screens/OrderListScreen.js
+++ frontend/src/screens/OrderListScreen.js
@@ -1,19 +1,22 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { listOrderMine } from '../actions/orderActions';
+import { listOrders } from '../actions/orderActions';
 import LoadingBox from '../components/LoadingBox';
 import MessageBox from '../components/MessageBox';
 
-export default function OrderHistoryScreen(props) {
-  const orderMineList = useSelector((state) => state.orderMineList);
-  const { loading, error, orders } = orderMineList;
+export default function OrderListScreen(props) {
+  const orderList = useSelector((state) => state.orderList);
+  const { loading, error, orders } = orderList;
   const dispatch = useDispatch();
   useEffect(() => {
-    dispatch(listOrderMine());
+    dispatch(listOrders());
   }, [dispatch]);
+  const deleteHandler = (order) => {
+    // TODO: delete handler
+  };
   return (
     <div>
-      <h1>Order History</h1>
+      <h1>Orders</h1>
       {loading ? (
         <LoadingBox></LoadingBox>
       ) : error ? (
@@ -23,6 +26,7 @@ export default function OrderHistoryScreen(props) {
           <thead>
             <tr>
               <th>ID</th>
+              <th>USER</th>
               <th>DATE</th>
               <th>TOTAL</th>
               <th>PAID</th>
@@ -34,6 +38,7 @@ export default function OrderHistoryScreen(props) {
             {orders.map((order) => (
               <tr key={order._id}>
                 <td>{order._id}</td>
+                <td>{order.user.name}</td>
                 <td>{order.createdAt.substring(0, 10)}</td>
                 <td>{order.totalPrice.toFixed(2)}</td>
                 <td>{order.isPaid ? order.paidAt.substring(0, 10) : 'No'}</td>
@@ -51,6 +56,13 @@ export default function OrderHistoryScreen(props) {
                     }}
                   >
                     Details
+                  </button>
+                  <button
+                    type="button"
+                    className="small"
+                    onclick={() => deleteHandler(order)}
+                  >
+                    Delete
                   </button>
                 </td>
               </tr>
