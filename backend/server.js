eab4d9d17998392e9463f8c8778e6f4855cde50d
wam <wam@onesty-tech.de>
2021-01-04 10:22:12 +0100

    publish frontend on heroku (update server.js port)

--- backend/server.js
+++ backend/server.js
@@ -40,6 +40,6 @@ app.use((err, req, res, next) => {
 })
 
 const port = process.env.PORT || 5000
-app.listen(5000, () => {
+app.listen(port, () => {
     console.log(`Serve at http://localhost:${port}`)
 })
